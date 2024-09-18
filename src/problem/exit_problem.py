6de5e5b691b831d1bddb95b5a71eab6fbdb4f888
MRoiban <roiban.marius.alexandru@gmail.com>
2024-09-18 08:58:32 +0200

    Add is_goal_state implementation in pseudocode to ExitProblem class

--- src/problem/exit_problem.py
+++ src/problem/exit_problem.py
@@ -8,4 +8,12 @@ class ExitProblem(SearchProblem[WorldState]):
     """
 
     def is_goal_state(self, state: WorldState) -> bool:
+        # if 
+            # 1. the robot is on the exit cell
+            # 2. the robot is alive
+        # then
+            # return true
+        # else
+            # return false
+         
         raise NotImplementedError()
